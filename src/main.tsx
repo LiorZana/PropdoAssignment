0f98ab2f039fa68daa4c7ba0e91e97c2e4ddb809
LiorZana <liorzana@gmail.com>
2022-02-28 21:00:21 +0200

    testing  fix

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+
+import { ChosenThemeProvider, ThemeProvider } from '@/providers'
+import App from './App'
+ReactDOM.render(
+  <StrictMode>
+    <ChosenThemeProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ChosenThemeProvider>
+  </StrictMode>,
+  document.getElementById('root')
+)
